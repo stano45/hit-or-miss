e9fb3876183eae1d4a397ed6fb144d49510d4cd0
Stanislav Kosorin <stanokosorin4@gmail.com>
2023-03-31 19:46:27 +0200

    Squash agane

--- src/bin/master.rs
+++ src/bin/master.rs
@@ -1,29 +1,25 @@
-use clap::Parser;
 use core::panic;
 use hash_ring::HashRing;
-use hitormiss::parser::{parse_request, Error, ParsedRequest};
+use hitormiss::parser::{
+    build_error_response, parse_request, CommandType, Error, ErrorCode, ParsedRequest,
+};
 use std::fmt;
-use std::sync::{Arc, Mutex};
-use tokio::io::AsyncWriteExt;
+use std::sync::Arc;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
 use tracing::{event, Level};
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Network port to use
-    #[arg(value_parser = clap::value_parser!(u16).range(1..))]
-    port: u16,
-}
 #[derive(Debug, Clone)]
 struct Partition {
+    addr: String,
     conn: Arc<Mutex<TcpStream>>,
 }
 
 impl fmt::Display for Partition {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        write!(fmt, "{}", self.conn.lock().unwrap().peer_addr().unwrap())
+        write!(fmt, "{}", self.addr)
     }
 }
 
@@ -35,9 +31,12 @@ async fn main() {
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
-    let addr = "127.0.0.1:6969";
-    event!(Level::INFO, "Starting master service on address: {addr}");
-    let listener = match TcpListener::bind(addr).await {
+    const MASTER_ADDR: &str = "127.0.0.1:6969";
+    event!(
+        Level::INFO,
+        "Starting master service on address: {MASTER_ADDR}"
+    );
+    let listener = match TcpListener::bind(MASTER_ADDR).await {
         Ok(listener) => {
             event!(Level::DEBUG, "{}", format!("Bind {:?}", listener));
             listener
@@ -48,9 +47,9 @@ async fn main() {
         }
     };
 
-    // how many times one node is replicated on the ring
-    let num_replicas = 10;
-    // set this to define some initial nodes (maybe when the drops and restarts or something idk)
+    // # of replicas per partition
+    let num_replicas = 1;
+    // set this to define some initial nodes
     let initial_nodes: Vec<Partition> = Vec::new();
     // consistent hashing node ring
     let ring: Ring = Arc::new(Mutex::new(HashRing::new(initial_nodes, num_replicas)));
@@ -73,12 +72,17 @@ async fn main() {
 
         let ring_clone: Ring = ring.clone();
         tokio::spawn(async move {
-            handle_connection(socket, ring_clone).await;
+            match handle_connection(socket, ring_clone).await {
+                Ok(_) => {}
+                Err(e) => {
+                    event!(Level::DEBUG, "Failed to handle connection: {}", e);
+                }
+            }
         });
     }
 }
 
-async fn handle_connection(socket: TcpStream, ring: Ring) {
+async fn handle_connection(mut socket: TcpStream, ring: Ring) -> Result<(), Error> {
     event!(
         Level::DEBUG,
         "{}",
@@ -86,55 +90,114 @@ async fn handle_connection(socket: TcpStream, ring: Ring) {
     );
 
     let mut buf = [0; 4096];
-    let request: Result<ParsedRequest, Error> = match socket.try_read(&mut buf) {
+    match socket.try_read(&mut buf) {
         Ok(_) => match parse_request(buf.to_vec()) {
             Ok(parsed_request) => {
                 event!(Level::DEBUG, "Parsed request: {:?}", parsed_request);
-                match parsed_request.cmd.as_str() {
-                    "NTF" => _handle_notify(socket, ring).await,
-                    "GET" => _handle_get(socket, std::str::from_utf8(&buf).unwrap(), ring).await,
-                    "SET" => _handle_set(socket, std::str::from_utf8(&buf).unwrap(), ring).await,
-                    "DEL" => _handle_delete(socket, std::str::from_utf8(&buf).unwrap(), ring).await,
-                    &_ => todo!(),
+                match parsed_request.cmd {
+                    CommandType::Get | CommandType::Set | CommandType::Delete => {
+                        forward_to_partition(socket, parsed_request, ring).await;
+                        Ok(())
+                    }
+                    CommandType::Notify => {
+                        handle_notify(socket, ring).await;
+                        Ok(())
+                    }
+                    CommandType::ListPartitions => {
+                        handle_list(socket, ring).await;
+                        Ok(())
+                    }
+                    CommandType::LSD => {
+                        unimplemented!("Send LSD to all partition and aggregate response");
+                    }
+                    CommandType::Hit | CommandType::Miss | CommandType::Error => {
+                        socket
+                            .write_all(&build_error_response(&Error::from_code(
+                                ErrorCode::UnsupportedCommandMaster,
+                            )))
+                            .await
+                            .unwrap();
+                        Ok(())
+                    }
                 }
-                Ok(parsed_request)
             }
             Err(e) => {
-                handle_error(socket, &e, ring).await;
+                socket.write_all(&build_error_response(&e)).await.unwrap();
                 Err(e)
             }
         },
         Err(e) => {
-            println!("error: {e}");
-            Err(Error {
-                code: 2, // Or another appropriate error code
-                msg: format!("Socket read error: {}", e),
-            })
+            event!(
+                Level::ERROR,
+                "Failed to read from socket: {:?} {:?}",
+                socket,
+                e
+            );
+            Err(Error::from_code(ErrorCode::FailedSocketRead))
         }
+    }
+}
+
+async fn forward_to_partition(mut client_socket: TcpStream, request: ParsedRequest, ring: Ring) {
+    let responsible_partition: Option<Partition> = if let Some(key) = request.key {
+        ring.lock()
+            .await
+            .get_node(key)
+            .map(|partition| partition.clone())
+    } else {
+        None
     };
-    event!(Level::DEBUG, "Request parsed: {:?}", request);
+    match responsible_partition {
+        Some(partition) => {
+            let mut partition_socket = partition.conn.lock().await;
+            partition_socket
+                .write_all(request.original_rq.as_bytes())
+                .await
+                .unwrap();
+            event!(
+                Level::DEBUG,
+                "Forwarded request to partition: {:?}",
+                partition.addr
+            );
+            let mut buf = [0; 4096];
+            partition_socket.read(&mut buf).await.unwrap();
+            event!(
+                Level::DEBUG,
+                "Got response from partition: {:?}: {}",
+                partition.addr,
+                String::from_utf8(buf.to_vec()).unwrap()
+            );
+            client_socket.write_all(&buf).await.unwrap();
+        }
+        None => {
+            client_socket
+                .write_all(&build_error_response(&Error::from_code(
+                    ErrorCode::NoPartition,
+                )))
+                .await
+                .unwrap();
+        }
+    }
 }
 
-async fn _handle_get(mut socket: TcpStream, buf: &str, _ring: Ring) {
-    socket.write_all(b"Here is the data\n").await.unwrap();
-    socket.write_all(buf.as_bytes()).await.unwrap();
+async fn handle_list(mut _socket: TcpStream, _ring: Ring) {
+    // let mut partitions = ring.lock().unwrap();
+    // let mut response = b"".to_vec();
+    // for partition in partitions {
+    //     response.extend_from_slice(partition.to_string().as_bytes());
+    //     response.extend_from_slice(b"");
+    // }
+    // client_socket.write_all(&response).await.unwrap();
+    // return;
 }
 
-async fn _handle_set(mut socket: TcpStream, buf: &str, _ring: Ring) {
-    socket.write_all(buf.as_bytes()).await.unwrap();
-}
-
-async fn _handle_delete(mut socket: TcpStream, buf: &str, _ring: Ring) {
-    socket.write_all(b"Deleted the data\n").await.unwrap();
-    socket.write_all(buf.as_bytes()).await.unwrap();
-}
-
-async fn _handle_notify(mut socket: TcpStream, ring: Ring) {
+async fn handle_notify(mut socket: TcpStream, ring: Ring) {
     let partition_addr = socket.peer_addr().unwrap();
     event!(Level::DEBUG, "NTF from partition: {:?}", partition_addr,);
     socket.write_all(b"ACK\n").await.unwrap();
 
-    ring.lock().unwrap().add_node(&Partition {
+    ring.lock().await.add_node(&Partition {
+        addr: partition_addr.to_string(),
         conn: Arc::new(Mutex::new(socket)),
     });
     event!(
@@ -142,10 +205,4 @@ async fn _handle_notify(mut socket: TcpStream, ring: Ring) {
         "Partition {:?} successfully added to ring",
         partition_addr,
     );
-}
-
-async fn handle_error(mut socket: TcpStream, err: &Error, _ring: Ring) {
-    let mut message = b"Error: ".to_vec();
-    message.extend_from_slice(err.msg.as_bytes());
-    socket.write_all(&message).await.unwrap();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,101 +1,182 @@
 use std::fmt;
+use std::str;
 
-pub static ERR_INVALID_REQUEST_CMD: &str = "Invalid request: command not found";
+pub static ERR_UNKNOWN_COMMAND: &str = "Invalid request: command not found";
 pub static ERR_NOT_ENOUGH_ARGS: &str = "Invalid request: not enough arguments";
-pub static _ERR_INVALID_REQUEST_ARG: &[u8; 42] = b"Invalid request: invalid command arguments";
-pub static _ERR_INVALID_REQUEST_FLAG: &[u8; 32] = b"Invalid request: flags not found";
-pub static _ERR_INVALID_REQUEST_FORMAT: &[u8; 39] = b"Invalid request: invalid UTF-8 sequence";
+pub static ERR_INVALID_ARGS: &str = "Invalid request: invalid command arguments";
+pub static ERR_INVALID_SEQUENCE: &str = "Invalid request: invalid UTF-8 sequence";
+pub static ERR_SOCKET_READ: &str = "Internal error: could not read from socket";
+pub static ERR_UNKNOWN: &str = "Invalid request: invalid UTF-8 sequence";
+pub static ERR_NO_PARTITION: &str = "Internal error: no partition found";
+pub static ERR_UNSUPPORTED_MASTER: &str =
+    "Internal error: this command is not supported on this node (master)";
+pub static ERR_UNSUPPORTED_PARTITION: &str =
+    "Internal error: this command is not supported on this node (partition)";
 
 #[derive(Debug, Clone)]
 pub struct Error {
-    pub code: u8,
+    pub code: ErrorCode,
     pub msg: String,
 }
-impl fmt::Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        write!(fmt, "Error {}: {}", self.code, self.msg)
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ErrorCode {
+    InvalidRequestCmd = 1,
+    NotEnoughArgs = 2,
+    InvalidRequestArg = 3,
+    InvalidSequence = 4,
+    FailedSocketRead = 5,
+    NoPartition = 6,
+    UnsupportedCommandMaster = 7,
+    UnsupportedCommandPartition = 8,
+    Unknown = 9,
+}
+
+impl ErrorCode {
+    pub fn from_u8(value: u8) -> Self {
+        match value {
+            1 => ErrorCode::InvalidRequestCmd,
+            2 => ErrorCode::NotEnoughArgs,
+            3 => ErrorCode::InvalidRequestArg,
+            4 => ErrorCode::InvalidSequence,
+            5 => ErrorCode::FailedSocketRead,
+            6 => ErrorCode::NoPartition,
+            7 => ErrorCode::UnsupportedCommandMaster,
+            8 => ErrorCode::UnsupportedCommandPartition,
+            _ => ErrorCode::Unknown,
+        }
+    }
+    pub fn to_u8(&self) -> u8 {
+        match self {
+            ErrorCode::InvalidRequestCmd => 1,
+            ErrorCode::NotEnoughArgs => 2,
+            ErrorCode::InvalidRequestArg => 3,
+            ErrorCode::InvalidSequence => 4,
+            ErrorCode::FailedSocketRead => 5,
+            ErrorCode::NoPartition => 6,
+            ErrorCode::UnsupportedCommandMaster => 7,
+            ErrorCode::UnsupportedCommandPartition => 8,
+            ErrorCode::Unknown => 9,
+        }
     }
 }
 
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl Error {
+    pub fn from_code(code: ErrorCode) -> Self {
+        let msg = match code {
+            ErrorCode::InvalidRequestCmd => ERR_UNKNOWN_COMMAND.to_string(),
+            ErrorCode::NotEnoughArgs => ERR_NOT_ENOUGH_ARGS.to_string(),
+            ErrorCode::InvalidRequestArg => ERR_INVALID_ARGS.to_string(),
+            ErrorCode::InvalidSequence => ERR_INVALID_SEQUENCE.to_string(),
+            ErrorCode::FailedSocketRead => ERR_SOCKET_READ.to_string(),
+            ErrorCode::NoPartition => ERR_NO_PARTITION.to_string(),
+            ErrorCode::UnsupportedCommandMaster => ERR_UNSUPPORTED_MASTER.to_string(),
+            ErrorCode::UnsupportedCommandPartition => ERR_UNSUPPORTED_PARTITION.to_string(),
+            ErrorCode::Unknown => ERR_UNKNOWN.to_string(),
+        };
+
+        Error { code, msg }
+    }
+    pub fn from_u8(value: u8) -> Self {
+        Self::from_code(ErrorCode::from_u8(value))
+    }
+}
+
+// Add CommandType enum
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CommandType {
+    Get,
+    Delete,
+    Set,
+    Notify,
+    ListPartitions,
+    LSD,
+    Hit,
+    Miss,
+    Error,
+}
 #[derive(Debug, Clone)]
 pub struct ParsedRequest {
-    pub cmd: String,
+    pub cmd: CommandType,
     pub key: Option<String>,
     pub value: Option<String>,
     pub error: Option<Error>,
+    pub original_rq: String,
 }
 pub fn parse_request(mut message: Vec<u8>) -> Result<ParsedRequest, Error> {
     if let Some(pos) = message.iter().position(|&c| c == b'\0') {
         message.truncate(pos);
     }
 
-    let buf = std::str::from_utf8(&message).map_err(|_| Error {
-        code: 1,
-        msg: "XD".to_string(),
-    })?;
-    let parts: Vec<&str> = buf.splitn(3, char::is_whitespace).collect();
+    let buf =
+        std::str::from_utf8(&message).map_err(|_| Error::from_code(ErrorCode::InvalidSequence))?;
+    let mut parts: Vec<&str> = buf.splitn(3, char::is_whitespace).collect();
+    if parts.last() == Some(&"") || parts.last() == Some(&"\n") || parts.last() == Some(&"\0") {
+        parts.pop();
+    }
 
     let cmd = extract_cmd(&parts)?;
 
-    let key = match cmd.as_str() {
-        "GET" | "SET" | "DEL" => extract_key(&parts),
+    let key = match cmd {
+        CommandType::Get | CommandType::Set | CommandType::Delete => extract_key(&parts),
         _ => Ok(None),
     }?;
 
-    let value = match cmd.as_str() {
-        "SET" => extract_value(&parts),
+    let value = match cmd {
+        CommandType::Set => extract_value(&parts),
         _ => Ok(None),
     }?;
 
-    let error = match cmd.as_str() {
-        "ERR" => extract_error(&parts),
+    let error = match cmd {
+        CommandType::Error => extract_error(&parts),
         _ => Ok(None),
     }?;
+
     Ok(ParsedRequest {
         cmd,
         key,
         value,
         error,
+        original_rq: buf.to_string(),
     })
 }
 
 fn extract_error(parts: &Vec<&str>) -> Result<Option<Error>, Error> {
     if parts.len() < 3 {
-        Err(Error {
-            code: 1,
-            msg: ERR_NOT_ENOUGH_ARGS.to_string(),
-        })
+        Err(Error::from_code(ErrorCode::NotEnoughArgs))
     } else {
-        Ok(Some(Error {
-            code: parts[1].parse().unwrap(),
-            msg: parts[2].to_string(),
-        }))
+        Ok(Some(Error::from_u8(parts[1].parse().unwrap())))
     }
 }
 
-fn extract_cmd(parts: &Vec<&str>) -> Result<String, Error> {
+fn extract_cmd(parts: &Vec<&str>) -> Result<CommandType, Error> {
     if parts.is_empty() {
-        Err(Error {
-            code: 1,
-            msg: ERR_INVALID_REQUEST_CMD.to_string(),
-        })
+        Err(Error::from_code(ErrorCode::NotEnoughArgs))
     } else {
         match parts[0] {
-            "GET" | "DEL" | "SET" | "NTF" | "LSP" | "LSD" | "ERR" => Ok(parts[0].to_string()),
-            _ => Err(Error {
-                code: 1,
-                msg: ERR_INVALID_REQUEST_CMD.to_string(),
-            }),
+            "GET" => Ok(CommandType::Get),
+            "DEL" => Ok(CommandType::Delete),
+            "SET" => Ok(CommandType::Set),
+            "NTF" => Ok(CommandType::Notify),
+            "LSP" => Ok(CommandType::ListPartitions),
+            "LSD" => Ok(CommandType::LSD),
+            "HIT" => Ok(CommandType::Hit),
+            "MSS" => Ok(CommandType::Miss),
+            "ERR" => Ok(CommandType::Error),
+            _ => Err(Error::from_code(ErrorCode::InvalidRequestCmd)),
         }
     }
 }
 
 fn extract_key(parts: &Vec<&str>) -> Result<Option<String>, Error> {
     if parts.len() < 2 {
-        Err(Error {
-            code: 1,
-            msg: ERR_NOT_ENOUGH_ARGS.to_string(),
-        })
+        Err(Error::from_code(ErrorCode::NotEnoughArgs))
     } else if parts[0].to_uppercase() == "GET" && parts.len() >= 3 {
         let key = format!("{} {}", parts[1], parts[2]).replace("\\0", "\u{0000}");
         Ok(Some(key))
@@ -106,11 +187,24 @@ fn extract_key(parts: &Vec<&str>) -> Result<Option<String>, Error> {
 
 fn extract_value(parts: &Vec<&str>) -> Result<Option<String>, Error> {
     if parts.len() < 3 {
-        Err(Error {
-            code: 1,
-            msg: ERR_NOT_ENOUGH_ARGS.to_string(),
-        })
+        Err(Error::from_code(ErrorCode::NotEnoughArgs))
     } else {
         Ok(Some(parts[2].to_string()))
     }
+}
+
+pub fn build_hit_response(key: &str, value: &str) -> Vec<u8> {
+    format!("HIT {} {}\0", key, value).into_bytes()
+}
+
+pub fn build_miss_response(key: &str) -> Vec<u8> {
+    format!("MSS {}\0", key).into_bytes()
+}
+
+pub fn build_ok_response() -> Vec<u8> {
+    "OK\0".to_string().into_bytes()
+}
+
+pub fn build_error_response(err: &Error) -> Vec<u8> {
+    format!("ERR {} {}\0", err.code.to_u8(), err.msg).into_bytes()
 }
